dbdab0018217a1918ca5f8124edf138c4a838e94
thiagomroato <65723256+thiagomroato@users.noreply.github.com>
2026-02-24 10:55:40 -0400

    Initialize Firebase app and Firestore database

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+const firebaseConfig = {
+   apiKey: "AIzaSyChd7Fs6qstzc3uMdOrcoC-4KMWH24MqZw",
+  authDomain: "financial-dashboard-a2b16.firebaseapp.com",
+  projectId: "financial-dashboard-a2b16",
+  storageBucket: "financial-dashboard-a2b16.firebasestorage.app",
+  messagingSenderId: "538205467615",
+  appId: "1:538205467615:web:24326fa7f323902059ca6a",
+  measurementId: "G-6JXMBQH9G1"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
